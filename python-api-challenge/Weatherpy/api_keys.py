1821c4cf72b1873ab4bf19e9b5d89ba9c8a7e72b
Melat Bekele <27melat28@gmail.com>
2021-03-14 03:27:50 -0400

    adding weather data

--- python-api-challenge/Weatherpy/api_keys.py
+++ python-api-challenge/Weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "08728989b0687b875ff513b21bfd8146"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCnnKK7GcsyJgV4ss45Avxzg3tytm-WE8s"

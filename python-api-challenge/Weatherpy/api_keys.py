d5480c112fb4e857faceb0ee96b184c5a9519f0d
MelatBekele <78058338+MelatBekele@users.noreply.github.com>
2021-03-18 23:47:10 -0400

    Update api_keys.py

--- python-api-challenge/Weatherpy/api_keys.py
+++ python-api-challenge/Weatherpy/api_keys.py
@@ -1,7 +1,7 @@
 # OpenWeatherMap API Key
-#weather_api_key = "ff787591babd86e5a266904737f0e3ac"
-weather_api_key = "08728989b0687b875ff513b21bfd8146"
-#weather_api_key= "e749c1422cd4cc916d8b668aea2a348d"
+#weather_api_key = 
+weather_api_key = 
+#weather_api_key= 
 
 # Google API Key
-g_key = "AIzaSyC6nr_Q-aoS1zyGMEP394xL3UvuOUhMBPo"
+g_key =
